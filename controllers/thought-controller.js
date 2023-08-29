48756c752dd787efffe5a880c7d51d105cf200e3
Andy Lin <andylin844@gmail.com>
2023-08-29 11:25:58 -0400

    final final edits

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -34,7 +34,7 @@ const ThoughtController = {
         Thought.create(body)
             .then(({ _id }) => {
                 return User.findOneAndUpdate(
-                    { _id: params.userId },
+                    { _id: body.userId },
                     { $push: { thoughts: _id } },
                     { new: true }
                 );
@@ -44,6 +44,21 @@ const ThoughtController = {
                     res.status(404).json({ message: 'No User found with this id! first error' });
                     return;
                 }
+                res.json(dbUserData);
+            })
+            .catch(err => res.json(err));
+    },
+    // update thought
+    updateThought( req , res) {
+        Thought.findOneAndUpdate({ _id: req.params.thoughtId }, {$set:req.body},{new: true, runValidators: true} )
+    
+            .then(updatethought => {
+                if (!updatethought) {
+                    return res.status(404).json({ message: 'No thought with this id!' });
+                }
+    
+            })
+            .then(dbUserData => {
                 res.json(dbUserData);
             })
             .catch(err => res.json(err));
@@ -87,7 +102,7 @@ const ThoughtController = {
         Thought.findOneAndUpdate(
             { _id: params.thoughtId },
             { $pull: { reactions: { reactionId: params.reactionId } } },
-            { new: true }
+            {new: true, runValidators: true} 
         )
             .then(dbUserData => res.json(dbUserData))
             .catch(err => res.json(err));
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -5,27 +5,31 @@ const {
     addThought,
     removeThought,
     addReaction,
+    updateThought,
     removeReaction
 } = require('../../controllers/thought-controller')
 
 router
     .route('/')
     .get(getAllThoughts)
-    
+    .post(addThought)
 
-router
-    .route('/:userId')
-    .post(addThought);
+
 router
     .route('/:thoughtId')
     .get(getThoughtById)
-    .put(addThought)
+    .put(updateThought)
     .delete(removeThought)
 
 router
-// /api/thoughts/:thoughtId/reactions
+    // /api/thoughts/:thoughtId/reactions
     .route('/:thoughtId/reactions')
     .post(addReaction)
+
+
+router
+// delete reaction
+    .route('/:thoughtId/reactions/:reactionId')
     .delete(removeReaction)
 
 module.exports = router;
